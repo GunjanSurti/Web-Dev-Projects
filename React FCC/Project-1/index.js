6aca4891888546b388cffcaccefa008003561e81
Gunjan Kalpeshkumar Surti <85667477+GunjanSurti@users.noreply.github.com>
2023-01-25 15:38:52 +0530

    Add files via upload

---
+++ React FCC/Project-1/index.js
@@ -0,0 +1,8 @@
+// ReactDOM.render(<h1>Hello Everyone!</h1>(tag), (place) document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>Gunjan</li>
+    <li>Surti</li>
+  </ul>,
+  document.getElementById("root")
+);
